6d2983c5bb2aa70fc865bc30d5109b4e824c1516
Tyke1109 <tygie1109@gmail.com>
2026-07-17 20:04:59 +0530

    Setup project structure and navigation

---
+++ src/screens/organizer/OrganizerDashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function OrganizerDashboard() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Organizer Dashboard</Text>
+    </View>
+  );
+}
