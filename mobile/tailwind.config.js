e1585ecbcdfe7aa34c582c138531e9a96cb711c0
Tyke1117 <chudasamapari1109@gmail.com>
2026-07-20 12:22:23 +0530

    Delete mobile directory

--- mobile/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./App.{js,jsx}", "./src/**/*.{js,jsx}"],
-  presets: [require("nativewind/preset")],
-  theme: { extend: {} },
-  plugins: [],
-}
